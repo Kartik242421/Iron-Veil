fca3769da825da8afbeacc900da30db701907966
Gauraang Goel <ggauraang@gmail.com>
2024-05-11 16:38:29 +0530

    Main Menu UI Updated

---
+++ Assets/Scripts/UIDisable.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class UIDisable : MonoBehaviour
+{
+    public GameObject gameObjectToControl;
+
+    void Start()
+    {
+        // Check if the platform is mobile
+        bool isMobilePlatform = Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer;
+
+        // Enable or disable the GameObject based on the platform
+        gameObjectToControl.SetActive(isMobilePlatform);
+    }
+}
