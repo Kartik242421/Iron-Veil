250667dd63bd746ae2223fb59fed0e8086ccf16b
Gauraang Goel <ggauraang@gmail.com>
2024-05-11 15:42:55 +0530

    MainMenu Script Edited

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,40 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private int PlayerSceneIndex = 3;
+    [SerializeField] private int optionScreenSceneIndex = 3;
+    [SerializeField] private int controlsScreenSceneIndex = 2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    public void OnePlayer()
+
+    public void OnePlayer(int Scene)
     {
         SaveHealthData.Player1Mode = true;
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(PlayerSceneIndex);
     }
+
     public void TwoPlayer()
     {
         SaveHealthData.Player1Mode = false;
-        SceneManager.LoadScene(1);
-
+        SceneManager.LoadScene(PlayerSceneIndex);
     }
+
     public void OptionScreen()
     {
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(optionScreenSceneIndex);
     }
+
     public void Exit()
     {
         Application.Quit();
     }
+
     public void ControlsScreen()
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(controlsScreenSceneIndex);
     }
 }
